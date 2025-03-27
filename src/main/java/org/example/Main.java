65fb56c671c447ea4b46775e0d271f11db0f3018
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-03-27 16:58:16 +0500

    java xml config1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,23 @@
 package org.example;
 
 
+import org.example.beans.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc-config.xml");
+        Person person1 = context.getBean("person", Person.class);
+        Person person2 = context.getBean("person", Person.class);
+        Person person3 = context.getBean("person", Person.class);
 
+        person1.setAge(20);
+        person1.setFullName("John Doe");
+
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
 
     }
 }
---
+++ src/main/java/org/example/beans/Person.java
@@ -0,0 +1,16 @@
+package org.example.beans;
+
+
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+      private int age;
+          private String fullName;
+}
