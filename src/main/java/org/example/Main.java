641e531bd12e23d012cb73dc531b67ddd11d8aed
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2025-03-28 18:36:38 +0500

    java xml config1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 package org.example;
 
 
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+import java.util.Arrays;
+
 public class Main {
+        static ExpressionParser parser = new SpelExpressionParser();
     public static void main(String[] args) {
 
+        Expression expression = parser.parseExpression("'Hello'");
+        String value = expression.getValue(String.class);
+        System.out.println(value);
 
+
+        Expression expression1 = ge("{1,2,3,4,5,6,7,8,9,10}");
+        int[] value1 = expression1.getValue(int[].class);
+        System.out.println(Arrays.toString(value1));
+
+
+    }
+    private static Expression ge(String expression) {
+        return parser.parseExpression(expression);
     }
 }
